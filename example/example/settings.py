677dcc5668e8182cdb4438c33fa0d13ffb8f894e
John Franey <johnfraney@gmail.com>
2018-05-17 00:28:53 -0300

    Add DRF to example project settings

--- example/example/settings.py
+++ example/example/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # if your app has other dependencies that need to be added to the site
     # they should be added here
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
